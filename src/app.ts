99efdfa738af17caf61e75eb4d8f9b979f7e2f61
Marko Rusic <markousp5@gmail.com>
2019-08-16 00:18:53 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+const main = () => {
+  document.querySelector('#app').innerHTML = `
+    <h1>ts-starter</h1>
+    <p>Change your src/app.ts</p>
+  `
+}
+
+window.addEventListener('DOMContentLoaded', main)
+
+export default main
